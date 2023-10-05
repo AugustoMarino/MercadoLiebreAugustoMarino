f7dec6e70dd8886ba90906aa50b21a96b137051f
AugustoMarino <augustomarino07@hotmail.com>
2023-10-05 14:21:04 -0300

    actualizando sistema de archivos y agregando ejs

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const path = require("path");
+const main = require('./routes/main')
+
 const app = express();
 
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html") );
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html") );
-});
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html") );
-});
+app.use('/', main);
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
